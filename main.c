ee84c928abf5a6aaaf432e4239d7552397197a08
Hide4ki <32337381+Hide4ki@users.noreply.github.com>
2018-10-07 20:08:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -21,27 +21,31 @@ void processing_line(char *buf)
 	char *pt;	  //pointer tmp
 	char *now;	  //pointer to current character
 	int cnt;      //counter
+	int flag;   
+	flag = 0;
 	ps = 0;
 	cnt = 0;
 	now = buf-1;
 	do 
 	{
 		now++; //next character
-		if((*now < 'a' || *now > 'z' )&&(*now < 'A' || *now > 'Z'))//check end of word
+		if((*now < 'A' || *now > 'Z') && (*now < 'a' || *now > 'z') && (*now < '0' || *now > '9'))//check end of word
 		{
-			if(ps && !(cnt&1))//there is a word start and even number vowels
+			if(ps && !(cnt&1) && !flag)//there is a word start and even number vowels
 			{
 				pt = ps;//remember start is deleteing word
 				while(*ps++ = *now++);//deleting word
 				now = pt-1;//return pointer on correct character
 			}
+			flag = 0;
 			cnt = 0;//zeroing the counter
 			ps = 0;//zeroing the pointer start 
 		}
 		else
 		{
 			ps = ((!ps)?now:ps);//if there is no start, let's remeber start
-			cnt += *now  == 'a' | *now == 'e' | *now == 'y' | *now == 'u' | *now == 'i' | *now == 'o';//if character is vowels, then +1 to counter
+			if(*now>='0'&&*now<='9') flag = 1;
+			cnt += *now  == 'a' | *now == 'e' | *now == 'u' | *now == 'i' | *now == 'o';//if character is vowels, then +1 to counter
 		}
 	}while(*now);
 }
